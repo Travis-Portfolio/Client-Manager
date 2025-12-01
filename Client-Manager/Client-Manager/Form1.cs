a991037d75f4e3889ecbd7fea7e2357afa114b9a
deboartm <deboartm@miamioh.edu>
2025-12-01 16:38:47 -0500

    Connected form to MySQL database
    Implemented code to allow my program to connect to a local instance of MySQL. ** Note: connection string information removed for security purposes

--- Client-Manager/Client-Manager/Form1.cs
+++ Client-Manager/Client-Manager/Form1.cs
@@ -1,3 +1,5 @@
+using MySql.Data.MySqlClient;
+
 namespace Client_Manager
 {
     public partial class Form1 : Form
@@ -5,6 +7,30 @@ namespace Client_Manager
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectString = ; // connection string data removed for security purposes 
+                MySqlConnection connection = new MySqlConnection();
+                connection.ConnectionString = connectString;
+                connection.Open();
+
+                string sqlTest = "SELECT * FROM client_table";
+                MySqlCommand cmdTest = new MySqlCommand(sqlTest, connection);
+
+                MySqlDataReader reader = cmdTest.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    MessageBox.Show("Client " + reader["clientID"] + " " + reader["first_name"]);
+                }
+            }catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
